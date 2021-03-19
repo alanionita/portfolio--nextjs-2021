5a98cad6174383c0f5b16c7dafed6b9073d2e92c
Alan Ionita <alanionita@users.noreply.github.com>
2021-03-19 15:10:42 +0000

    FIX (#12) : removed the i18n/domains section of the config
    It was interfering with the exportPathMap

--- next.config.js
+++ next.config.js
@@ -2,11 +2,5 @@ module.exports = {
   i18n: {
     locales: ["en_GB"],
     defaultLocale: "en_GB",
-    domains: [
-      {
-        domain: "example.com",
-        defaultLocale: "en_GB",
-      },
-    ],
-  },
+  }
 };
