28322f722d13e07cd5a38d9ecd3dac2ec6a7c578
Alan Ionita <alanionita@users.noreply.github.com>
2021-03-19 14:19:00 +0000

    FEAT (#12) : adds a lang using the NextJS internationalisation features

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  i18n: {
+    locales: ["en_GB"],
+    defaultLocale: "en_GB",
+    domains: [
+      {
+        domain: "example.com",
+        defaultLocale: "en_GB",
+      },
+    ],
+  },
+};
