c6ab991d8bb9f545a41fbdbd439b5577567b8930
Alan Ionita <alanionita@users.noreply.github.com>
2021-03-19 15:12:13 +0000

    REFACTOR (#12) : refactored the lib/posts util
    Made use of the read feature from matter rather than using an older sample code

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import matter from "gray-matter";
+import { read } from "gray-matter";
 import remark from "remark";
 import html from "remark-html";
 import { Posts, Post } from "../types/posts";
@@ -9,18 +9,11 @@ import { dateSortDecending } from "../lib/utils";
 const postsDirectory = path.join(process.cwd(), "data", "posts");
 
 export function getSortedPosts(): Posts {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPosts = fileNames.map((fileName: string) => {
-    // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, "");
-
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+    const matterResult = read(fullPath);
     return {
       id,
       ...matterResult.data,
@@ -43,19 +36,11 @@ export function getAllPostIds() {
 
 export async function getPost(id: string | Array<string>) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
-
-  // Use remark to convert markdown into HTML string
+  const matterResult = read(fullPath);
   const processedContent = await remark()
     .use(html, { sanitize: true })
     .process(matterResult.content);
-
   const contentHtml = processedContent.toString();
-
-  // Combine the data with the id and contentHtml
   return {
     id,
     contentHtml,
