70a22902232eef81a325b572866d475f9cbcd6b4
Alan Ionita <alanionita@users.noreply.github.com>
2021-03-23 20:28:13 +0000

    FEAT (#19) : adds regression test for a few screen sizes

--- cypress/integration/visual.tests.spec.js
+++ cypress/integration/visual.tests.spec.js
@@ -1,15 +1,25 @@
-const pages = ["", "blog", "posts/pi-os-study"];
+const sizes = [
+  ["iphone-6", "landscape"],
+  "iphone-6",
+  "ipad-2",
+  ["ipad-2", "landscape"],
+  [1920, 1080],
+];
 
+const pages = ["", "blog", "posts/pi-os-study"];
 
 describe("Visual regression tests", () => {
   it("Should match the homepage", () => {
     cy.visit("/");
     cy.matchImageSnapshot();
   });
-  pages.forEach((page) => {
-    it(`Should match previous screenshot '${page} Page'`, () => {
-      cy.visit(`/${page}`);
-      cy.matchImageSnapshot();
+  sizes.forEach((size) => {
+    pages.forEach((page) => {
+      it(`Should match previous screenshot '${page} Page, when '${size}' resolution'`, () => {
+        cy.setResolution(size);
+        cy.visit(`/${page}`);
+        cy.matchImageSnapshot();
+      });
     });
   });
 });
