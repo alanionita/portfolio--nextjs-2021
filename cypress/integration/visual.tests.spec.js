d9873405c653e84dc2ba0ff26be81bbe8d40395f
Alan Ionita <alanionita@users.noreply.github.com>
2021-03-24 08:14:11 +0000

    REFACTOR : simplified the visual.tests.spec.js imports

--- cypress/integration/visual.tests.spec.js
+++ cypress/integration/visual.tests.spec.js
@@ -1,25 +1,17 @@
-const sizes = [
-  ["iphone-6", "landscape"],
-  "iphone-6",
-  "ipad-2",
-  ["ipad-2", "landscape"],
-  [1920, 1080],
-];
-
-const pages = ["", "blog", "posts/pi-os-study"];
+import { pages, sizes, scrubbedElements } from "../support/options";
 
 describe("Visual regression tests", () => {
-  it("Should match the homepage", () => {
-    cy.visit("/");
-    cy.matchImageSnapshot();
-  });
   sizes.forEach((size) => {
     pages.forEach((page) => {
       it(`Should match previous screenshot '${page} Page, when '${size}' resolution'`, () => {
+        const now = new Date(Date.UTC(2019, 1, 1)).getTime();
+        cy.clock(now);
         cy.setResolution(size);
         cy.visit(`/${page}`);
-        cy.matchImageSnapshot();
+        // cy.matchImageSnapshot();
+        cy.matchImageSnapshot({ blackout: scrubbedElements });
       });
     });
   });
 });
+  
