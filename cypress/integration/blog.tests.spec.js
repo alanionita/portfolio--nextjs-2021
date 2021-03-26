0ab764bc315ee49782a2f39c8697e45354608fe0
Alan Ionita <alanionita@users.noreply.github.com>
2021-03-26 23:24:36 +0000

    FEAT (test) : adds test that navigates through all the /blog posts, screenshotting each one of them

--- cypress/integration/blog.tests.spec.js
+++ cypress/integration/blog.tests.spec.js
@@ -4,27 +4,22 @@ describe("Visual regression on /posts/{id}", () => {
   sizes.forEach((size) => {
     it(`Should match previous screenshot blog Page, when '${size}' resolution'`, () => {
       cy.visit("/blog");
-      // cy.get(`ul`)
-      //   // .should("have.length", 9)
-      //   .children()
-      //   .toArray()
-      //   .forEach((el) => {
-      //     el.click();
-      //     cy.wait(1000);
-
-      cy.get("ul > li > a")
-        .should("have.length", 9)
-        .each((element) => {
-          cy.wrap(element)
-            .invoke("attr", "href")
-            .then((href) => {
-              cy.visit(href);
-            });
-          cy.wrap(element)
-          .should("have.attr", "href")
-          .and("include", "posts"  
-          cy.wait(1000);
-        });
+      cy.get("ul > li > a").should("have.length", 9);
+      cy.get("ul > li > a").each(($element) => {
+        cy.wrap($element)
+          .invoke("attr", "href")
+          .then((href) => {
+            cy.wrap(href).should("exist")
+            cy.visit(href);
+            cy.wait(2000);
+          });
+      });
+    });
+    it(`All posts should have the correct url`, () => {
+      cy.visit("/blog");
+      cy.get("ul > li > a").each(($element) => {
+        cy.wrap($element).should("have.attr", "href").and("include", "posts"
+      });
     });
   });
 });
