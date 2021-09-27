b4c82bd3c249e0312f370b9ed3295bf1cad711b0
Alan Ionita <8453106+alanionita@users.noreply.github.com>
2021-09-27 22:30:29 +0100

    FAT (#31) updates the styling of the blog page too
    Adding the max-width features
    
    Making it a bit more responsives

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent } from "react";
 import Link from "next/link";
 import { Date } from "../Date";
 import { TListProps } from "../../types/props";
@@ -7,18 +7,22 @@ import styles from "./list.module.css";
 export const List: FunctionComponent<TListProps> = ({ list }) => (
   <>
     {list && (
-      <ul className={styles.main}>
-        {list.map(({ id, title, url, created }) => (
-          <li className={styles.item} key={id}>
-            <Link href='/posts/[id]' as={`/posts/${url}`}>
-              <a>{title}</a>
-            </Link>
-            <small>
-              <Date dateString={created} />
-            </small>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.wrapper}>
+        <div className={styles.maxWidth}>
+          <ul className={styles.main}>
+            {list.map(({ id, title, url, created }) => (
+              <li className={styles.item} key={id}>
+                <Link href="/posts/[id]" as={`/posts/${url}`}>
+                  <a>{title}</a>
+                </Link>
+                <small>
+                  <Date dateString={created} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     )}
     {list && list.length < 1 && <p>No available articles, but stay tuned!</p>}
   </>
