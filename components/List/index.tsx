86eadb7dc34c539080ec9391a489728d5c97721e
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2023-01-02 01:22:47 +0000

    FIX : adds a data-type to the blog list

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -9,7 +9,7 @@ export const List: FunctionComponent<TListProps> = ({ list }) => (
     {list && (
       <div className={styles.wrapper}>
         <div className={styles.maxWidth}>
-          <ul className={styles.main}>
+          <ul className={styles.main} data-type="url-list">
             {list.map(({ id, title, url, created }) => (
               <li className={styles.item} key={id} >
                 <Link href="/posts/[id]" as={`/posts/${url}`} data-type="url">
